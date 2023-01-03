b2eec612bc300a9481f29a49a631ea8cfb898d69
Nayara de Souza <eng.nayara@gmail.com>
2023-01-02 22:13:27 -0300

    oepradores em java

---
+++ tipos-variaveis/src/edu/nayara/primeirasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.nayara.primeirasemana;
+
+public class MinhaClasse{
+    public static void main (String [] args){
+        
+        String primeiroNome = "Nayara";
+        String segundoNome = "de Souza";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+        
+}
